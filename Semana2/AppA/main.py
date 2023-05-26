4b3a9aeae155fa933c0f1e0e35c42e865e4c79ea
Carlos Gonzalez <gonzalezcarlos7684@gmail.com>
2023-05-25 21:33:13 -0600

    Codigo visto hoy

--- Semana2/AppA/main.py
+++ Semana2/AppA/main.py
@@ -1,4 +1,16 @@
-print("Hola mundo")
-print("Lica campeon")
-print("Saprissa campeon")
-print("Heredia campeon")
+def es_primo(numero):
+    '''Función para verificar si un número es primo'''
+    if numero < 2:
+        return False
+    for i in range(2, numero):
+        if numero % i == 0:
+            return False
+    return True
+
+limite = int(input("Ingrese el límite para evaluar números primos: "))
+
+print("Números primos hasta el límite", limite, ":")
+#2 ... 10 2-3-4-5
+for num in range(2, limite + 1):
+    if es_primo(num):
+        print(num)
